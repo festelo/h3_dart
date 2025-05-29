a2a811c4fe29741ec95965e1341192ea780d9a76
Ilya Beregovskiy <festeloqq@gmail.com>
2025-05-30 01:37:10 +0200

    h3_dart - upgrade to 7.0.0

--- h3_dart/example/ffi/src/main.dart
+++ h3_dart/example/ffi/src/main.dart
@@ -3,6 +3,6 @@ import 'package:h3_dart/h3_dart.dart';
 void main() {
   final h3 = H3Factory().byPath('../../../h3_ffi/c/h3lib/build/h3.so');
 
-  final isValid = h3.h3IsValid(BigInt.parse('0x85283473fffffff'));
+  final isValid = h3.isValidCell(BigInt.parse('0x85283473fffffff'));
   print('0x85283473fffffff is valid hex: $isValid');
 }
--- h3_dart/example/web/web/main.dart
+++ h3_dart/example/web/web/main.dart
@@ -1,10 +1,11 @@
-import 'dart:html';
+import 'package:web/web.dart';
 
 import 'package:h3_dart/h3_dart.dart';
 
 void main() {
   const h3Factory = H3Factory();
   final h3 = h3Factory.web();
-  final isValid = h3.h3IsValid(BigInt.parse('0x85283473fffffff'));
-  querySelector('#output')?.text = '0x85283473fffffff is valid hex: $isValid';
+  final isValid = h3.isValidCell(BigInt.parse('0x85283473fffffff'));
+  document.querySelector('#output')?.textContent =
+      '0x85283473fffffff is valid hex: $isValid';
 }
